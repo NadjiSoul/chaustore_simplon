adc176e7aae294ba2d9eef60bd3e90534678bf65
Soulaimana Nadji <nadji.soulaimana@gmail.com>
2019-10-18 10:48:46 +0200

    Revert "Delete connect.php"
    This reverts commit 77938905c7fa401c6c2f3adf144dcdf5e6b487ae.

--- includes/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$cnx = mysqli_connect("localhost", "nadji", "180791640"); //164.132.110.233 xCIwyTKo3)?(31;*
-$db = mysqli_select_db($cnx, "chaustore");
-mysqli_set_charset($cnx, 'utf8');
-
-?>
